df6b93eb9202939aca01a7e33465a2ac23727bd7
joewilson04 <joebot24@outlook.com>
2024-07-02 12:05:55 +0100

    created Input and NewProject

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,28 @@
+export default function NewProject() {
+  return (
+    <div>
+      <menu>
+        <li>
+          <button>Cancel</button>
+        </li>
+        <li>
+          <button>Save</button>
+        </li>
+      </menu>
+      <div>
+        <p>
+          <title>Title</title>
+          <input />
+        </p>
+        <p>
+          <lable>Description</lable>
+          <textarea />
+        </p>
+        <p>
+          <label>Due Date</label>
+          <input />
+        </p>
+      </div>
+    </div>
+  );
+}
